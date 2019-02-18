2454354a48f9600c094b4d66ca7761d11a52be0c
Luke Harris <lukeharris01170@gmail.com>
2019-02-18 23:29:57 +0000

    Test requests to netlify functions from client

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import Axios from "axios";
 
-function App() {
-  return <div>Test change</div>;
+class App extends Component {
+  state = { fetching: false, data: [], error: false };
+  fetchData = () => {
+    this.setState({ fetching: true, data: [], error: false });
+    Axios.get("/.netlify/functions/todos")
+      .then(response => {
+        this.setState({ fetching: false, data: response.data });
+      })
+      .catch(error => {
+        this.setState({ fetching: false, error });
+      });
+  };
+  render() {
+    return (
+      <div>
+        <pre>{JSON.stringify(this.state, null, 4)}</pre>
+        <button onClick={this.fetchData}>Fetch</button>
+      </div>
+    );
+  }
 }
 
 export default App;
