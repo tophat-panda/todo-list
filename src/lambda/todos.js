f8ad56052da554cb9c8f822ccb4fb4b233646e93
Luke Harris <lukeharris01170@gmail.com>
2019-02-18 21:18:51 +0000

    Create a post and get todo endpoint

--- src/lambda/todos.js
+++ src/lambda/todos.js
@@ -1,23 +1,79 @@
-const todos = [
-  {
-    id: "5349b4ddd2781d08c09890f3",
-    name: process.env.FOO,
-    completed: true
-  },
-  {
-    id: "54759eb3c090d83494e2d804",
-    name: "Do a hackathon",
-    completed: false
-  }
-];
+import * as mongo from "mongodb";
 
-export async function handler(event, context) {
-  // This code gets run on the server
+function response({ statusCode = 200, body }) {
   return {
-    statusCode: 200,
+    statusCode,
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(todos)
+    body: JSON.stringify(body)
   };
+}
+
+async function getDatabaseConnection() {
+  // Create a mongo client instance
+  const db = await mongo.MongoClient.connect(process.env.DATABASE);
+  // get the todo database
+  const dbo = db.db("todo");
+  // Return the dbo and a means to close the connection
+  return {
+    dbo,
+    close() {
+      return db.close();
+    }
+  };
+}
+
+async function createNewTodo({ name }) {
+  // Get a database connection
+  const { dbo, close } = await getDatabaseConnection();
+  // Get the todos collection from the dbo
+  const todos = dbo.collection("todos");
+  // Insert the new todo into the todos collection
+  const todo = await todos.insertOne({ name, done: false, due: new Date() });
+  // Close conection to database
+  await close();
+  // Return insert id of new todo
+  return response({ statusCode: 201, body: todo.insertedId });
+}
+
+async function getRecentTodos() {
+  // Get a database connection
+  const { dbo, close } = await getDatabaseConnection();
+  // Get the todos collection from the dbo
+  const todos = dbo.collection("todos");
+  // Find the 10 most recent todos from the todos collection
+  const recentTodos = await todos
+    .find()
+    .limit(10)
+    .toArray();
+  // Close conection to database
+  await close();
+  // Return the recent todos
+  return response({ body: recentTodos });
+}
+
+export async function handler(event, context) {
+  switch (event.httpMethod) {
+    case "POST":
+      try {
+        const todo = JSON.parse(event.body);
+        return createNewTodo(todo);
+      } catch (e) {
+        console.error(e);
+        if (e instanceof SyntaxError) {
+          return response({
+            statusCode: 400,
+            body: { message: "Bad request" }
+          });
+        }
+        return response({
+          statusCode: 500,
+          body: { message: "Unknown error" }
+        });
+      }
+
+    default:
+      return getRecentTodos();
+  }
 }
